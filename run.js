76ac8faf6a4701195e26ca4656621f6b7ea8828b
Phil Gambling <pgambling@gmail.com>
2015-05-20 20:44:37 -0500

    Clearing out CLJS code

--- run.js
+++
@@ -1,8 +0,0 @@
-try {
-    require("source-map-support").install();
-} catch(err) {
-}
-require("./out/goog/bootstrap/nodejs.js");
-require("./out/puzzle.js");
-goog.require("puzzle.core");
-goog.require("cljs.nodejscli");
